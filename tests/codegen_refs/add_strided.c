f2c170ce5b61b7d6ebcbb4568e0a13aad211e1b2
Timo Stripf <timo.stripf@emmtrix.com>
2026-01-03 01:11:53 +0100

    Relax codegen op constraints for matmul and bmm

---
+++ tests/codegen_refs/add_strided.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include "ops_scalar_f32.h"
+
+void node1_add_f32(const float a[3][2], const float b[3][2], float out[3][2]) {
+    for (int64_t i0 = 0; i0 < 3; ++i0) {
+        for (int64_t i1 = 0; i1 < 2; ++i1) {
+            out[i0][i1] = ref_scalar_f32_add(((float*)a)[i0 * 1 + i1 * 3], ((float*)b)[i0 * 1 + i1 * 3]);
+        }
+    }
+}
+
+void ref_codegen_main_f32(const float input_0[3][2], const float input_1[3][2], float out[3][2]) {
+    node1_add_f32(input_0, input_1, out);
+}
