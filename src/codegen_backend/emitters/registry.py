e25191e2cea133d5f97115758cb880dab37534b3
Timo Stripf <timo.stripf@emmtrix.com>
2026-01-07 09:21:49 +0100

    Refactor codegen emitters registry

--- src/codegen_backend/emitters/registry.py
+++ src/codegen_backend/emitters/registry.py
@@ -1,325 +1,41 @@
 from __future__ import annotations
 
-from typing import Dict, List
-
-from c_ref_backend.cffi_bindings import RefBackendError
+from typing import Dict
 from codegen_backend.emitters.arange import ArangeEmitter
 from codegen_backend.emitters.argreduction import ArgReductionEmitter
-from codegen_backend.emitters.base import KindEmitter, KindEmitterBase
+from codegen_backend.emitters.base import KindEmitter
 from codegen_backend.emitters.elementwise import ElementwiseEmitter
 from codegen_backend.emitters.flip import FlipEmitter
+from codegen_backend.emitters.legacy import (
+    AddbmmEmitter,
+    AddmmEmitter,
+    AddmvEmitter,
+    AddrEmitter,
+    BatchNormEmitter,
+    CdistEmitter,
+    Col2imEmitter,
+    ConcatEmitter,
+    Conv1dEmitter,
+    Conv2dEmitter,
+    CumsumEmitter,
+    DiagonalEmitter,
+    EmbeddingBagEmitter,
+    EmbeddingEmitter,
+    EmptyStridedEmitter,
+    GatherEmitter,
+    PdistEmitter,
+    Pool1dEmitter,
+    Pool2dBackwardEmitter,
+    Pool2dEmitter,
+    Pool3dEmitter,
+    SoftmaxEmitter,
+)
 from codegen_backend.emitters.matmul import MatmulEmitter
 from codegen_backend.emitters.pad import PadEmitter
 from codegen_backend.emitters.reduction import ReductionEmitter
 from codegen_backend.emitters.resize import ResizeEmitter
 from codegen_backend.emitters.view import ViewEmitter
-from codegen_backend.kinds import KernelEmitRequest
 from codegen_backend.specs import OpKind
-
-
-class TemporaryEmitter(KindEmitterBase):
-    def __init__(self, kind: OpKind) -> None:
-        self._kind = kind
-
-    def emit(self, req: KernelEmitRequest) -> List[str]:
-        from codegen_backend import backend as backend_module
-
-        kind = self._kind
-        if kind == OpKind.EMPTY_STRIDED:
-            return backend_module._write_empty_strided_kernel(
-                req.node_index,
-                req.op_spec,
-                req.output_shape,
-                req.dtype,
-            )
-        if kind == OpKind.DIAGONAL:
-            input_shape = req.input_shapes[0]
-            input_strides = req.input_strides[0]
-            input_dtype = req.input_dtypes[0]
-            return backend_module._write_diagonal_kernel(
-                req.node_index,
-                req.op_node,
-                input_shape,
-                input_strides,
-                input_dtype,
-                req.output_shape,
-                req.output_strides,
-                req.dtype,
-            )
-        if kind == OpKind.SOFTMAX:
-            return backend_module._write_softmax_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_strides[0],
-                req.output_strides,
-                int(req.params["dim"]),
-                req.dtype,
-            )
-        if kind == OpKind.CUMSUM:
-            return backend_module._write_cumsum_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_strides[0],
-                req.output_strides,
-                int(req.params["dim"]),
-                req.dtype,
-                req.params["output_dtype"],
-            )
-        if kind == OpKind.EMBEDDING:
-            return backend_module._write_embedding_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_shapes[1],
-                req.input_strides[0],
-                req.input_strides[1],
-                req.output_shape,
-                req.output_strides,
-                req.input_dtypes[1],
-                req.dtype,
-                padding_idx=int(req.params.get("padding_idx", -1)),
-            )
-        if kind == OpKind.EMBEDDING_BAG:
-            return backend_module._write_embedding_bag_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_shapes[1],
-                req.input_shapes[2],
-                req.input_strides[0],
-                req.input_strides[1],
-                req.input_strides[2],
-                req.output_shape,
-                req.output_strides,
-                req.input_dtypes[1],
-                req.input_dtypes[2],
-                req.dtype,
-                mode=int(req.params.get("mode", 0)),
-                padding_idx=int(req.params.get("padding_idx", -1)),
-                include_last_offset=bool(
-                    req.params.get("include_last_offset", False)
-                ),
-            )
-        if kind == OpKind.GATHER:
-            return backend_module._write_gather_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_shapes[1],
-                req.input_strides[0],
-                req.input_strides[1],
-                req.output_shape,
-                req.output_strides,
-                req.input_dtypes[1],
-                int(req.params["dim"]),
-                req.dtype,
-            )
-        if kind == OpKind.CONCAT:
-            return backend_module._write_concat_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes,
-                req.input_strides,
-                req.output_shape,
-                req.output_strides,
-                int(req.params.get("dim", 0)),
-                req.dtype,
-            )
-        if kind == OpKind.POOL2D:
-            return backend_module._write_pool2d_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.params["kernel_size"],
-                req.params["stride"],
-                req.params["padding"],
-                req.params["dilation"],
-                req.dtype,
-                bool(req.params.get("ceil_mode", False)),
-                bool(req.params.get("count_include_pad", False)),
-                req.params.get("divisor_override"),
-            )
-        if kind == OpKind.POOL3D:
-            return backend_module._write_pool3d_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.params["kernel_size"],
-                req.params["stride"],
-                req.params["padding"],
-                req.params["dilation"],
-                req.dtype,
-                bool(req.params.get("ceil_mode", False)),
-                bool(req.params.get("count_include_pad", False)),
-                req.params.get("divisor_override"),
-            )
-        if kind == OpKind.POOL2D_BACKWARD:
-            return backend_module._write_adaptive_avg_pool2d_backward_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_shapes[1],
-                req.output_shape,
-                req.params["kernel_size"],
-                req.params["stride"],
-                req.dtype,
-            )
-        if kind == OpKind.POOL1D:
-            return backend_module._write_pool1d_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.params["kernel_size"],
-                req.params["stride"],
-                req.params["padding"],
-                req.params["dilation"],
-                req.dtype,
-                bool(req.params.get("ceil_mode", False)),
-                bool(req.params.get("count_include_pad", False)),
-                req.params.get("divisor_override"),
-            )
-        if kind == OpKind.COL2IM:
-            return backend_module._write_col2im_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.params["output_size"],
-                req.params["kernel_size"],
-                req.params["dilation"],
-                req.params["padding"],
-                req.params["stride"],
-                req.dtype,
-                int(req.params.get("out_blocks_h", 1)),
-                int(req.params.get("out_blocks_w", 1)),
-            )
-        if kind == OpKind.BATCH_NORM:
-            return backend_module._write_batch_norm_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.dtype,
-                float(req.params.get("eps", 1e-5)),
-                bool(req.params.get("has_weight", False)),
-                bool(req.params.get("has_bias", False)),
-            )
-        if kind == OpKind.PDIST:
-            return backend_module._write_pdist_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.dtype,
-            )
-        if kind == OpKind.CDIST:
-            return backend_module._write_cdist_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_shapes[1],
-                req.output_shape,
-                req.dtype,
-            )
-        if kind == OpKind.CONV1D:
-            return backend_module._write_conv1d_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_shapes[1],
-                req.output_shape,
-                int(req.params.get("stride", 1)),
-                int(req.params.get("padding", 0)),
-                int(req.params.get("dilation", 1)),
-                int(req.params.get("groups", 1)),
-                req.dtype,
-                bool(req.params.get("has_bias", False)),
-            )
-        if kind == OpKind.CONV2D:
-            return backend_module._write_conv2d_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.input_shapes[1],
-                req.output_shape,
-                bool(req.params.get("transposed", False)),
-                req.params.get("stride", (1, 1)),
-                req.params.get("padding", (0, 0)),
-                req.params.get("dilation", (1, 1)),
-                int(req.params.get("groups", 1)),
-                req.dtype,
-                bool(req.params.get("has_bias", False)),
-            )
-        if kind == OpKind.ADDMM:
-            return backend_module._write_addmm_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.input_shapes[1],
-                req.input_shapes[2],
-                req.input_strides[0],
-                req.input_strides[1],
-                req.input_strides[2],
-                req.output_strides,
-                req.dtype,
-                alpha=float(req.params.get("alpha", 1.0)),
-                beta=float(req.params.get("beta", 1.0)),
-            )
-        if kind == OpKind.ADDBMM:
-            return backend_module._write_addbmm_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.input_shapes[1],
-                req.input_shapes[2],
-                req.input_strides[0],
-                req.input_strides[1],
-                req.input_strides[2],
-                req.output_strides,
-                req.dtype,
-                alpha=float(req.params.get("alpha", 1.0)),
-                beta=float(req.params.get("beta", 1.0)),
-            )
-        if kind == OpKind.ADDMV:
-            return backend_module._write_addmv_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.input_shapes[1],
-                req.input_shapes[2],
-                req.input_strides[0],
-                req.input_strides[1],
-                req.input_strides[2],
-                req.output_strides,
-                req.dtype,
-                alpha=float(req.params.get("alpha", 1.0)),
-                beta=float(req.params.get("beta", 1.0)),
-            )
-        if kind == OpKind.ADDR:
-            return backend_module._write_addr_kernel(
-                req.node_index,
-                req.op_spec,
-                req.input_shapes[0],
-                req.output_shape,
-                req.input_shapes[1],
-                req.input_shapes[2],
-                req.input_strides[0],
-                req.input_strides[1],
-                req.input_strides[2],
-                req.output_strides,
-                req.dtype,
-                alpha=float(req.params.get("alpha", 1.0)),
-                beta=float(req.params.get("beta", 1.0)),
-            )
-        raise RefBackendError(f"Unsupported kernel kind: {kind.value}")
 
 
 def build_kind_emitters() -> Dict[OpKind, KindEmitter]:
@@ -331,20 +47,37 @@ def build_kind_emitters() -> Dict[OpKind, KindEmitter]:
         OpKind.WHERE,
     }
     kind_emitters: Dict[OpKind, KindEmitter] = {
+        OpKind.EMPTY_STRIDED: EmptyStridedEmitter(),
         OpKind.ARANGE: ArangeEmitter(),
         OpKind.ARG_REDUCTION: ArgReductionEmitter(),
+        OpKind.DIAGONAL: DiagonalEmitter(),
+        OpKind.SOFTMAX: SoftmaxEmitter(),
+        OpKind.CUMSUM: CumsumEmitter(),
+        OpKind.CONCAT: ConcatEmitter(),
         OpKind.FLIP: FlipEmitter(),
+        OpKind.EMBEDDING: EmbeddingEmitter(),
+        OpKind.EMBEDDING_BAG: EmbeddingBagEmitter(),
+        OpKind.GATHER: GatherEmitter(),
         OpKind.MATMUL: MatmulEmitter(),
+        OpKind.ADDMM: AddmmEmitter(),
+        OpKind.ADDBMM: AddbmmEmitter(),
+        OpKind.ADDMV: AddmvEmitter(),
+        OpKind.ADDR: AddrEmitter(),
+        OpKind.CONV1D: Conv1dEmitter(),
+        OpKind.CONV2D: Conv2dEmitter(),
+        OpKind.POOL1D: Pool1dEmitter(),
+        OpKind.POOL2D: Pool2dEmitter(),
+        OpKind.POOL3D: Pool3dEmitter(),
+        OpKind.POOL2D_BACKWARD: Pool2dBackwardEmitter(),
+        OpKind.COL2IM: Col2imEmitter(),
+        OpKind.BATCH_NORM: BatchNormEmitter(),
+        OpKind.PDIST: PdistEmitter(),
+        OpKind.CDIST: CdistEmitter(),
         OpKind.PAD: PadEmitter(),
         OpKind.REDUCTION: ReductionEmitter(),
         OpKind.VIEW: ViewEmitter(),
         OpKind.RESIZE: ResizeEmitter(),
     }
-    return {
-        kind: (
-            elementwise_emitter
-            if kind in elementwise_kinds
-            else kind_emitters.get(kind, TemporaryEmitter(kind))
-        )
-        for kind in OpKind
-    }
+    for kind in elementwise_kinds:
+        kind_emitters[kind] = elementwise_emitter
+    return {kind: kind_emitters[kind] for kind in OpKind}
